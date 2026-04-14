5d772430b740ecde4e54e89e185d965826f6a95c
Gustav O <ac75309@skola.goteborg.se>
2026-04-14 22:48:05 +0200

    First component, navigation

--- components/PreferenceButton.js
+++ components/PreferenceButton.js
@@ -3,8 +3,13 @@ import { ref } from "vue";
 export default {
     name: "PreferenceButton",
 
+    setup() {
+        return {};
+    },
+
     template: `
-        <button class="preference">
+        <button 
+            class="preferenceButton">
             <slot></slot>
         </button>
     `
