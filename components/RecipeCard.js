7ce0461624b4cd83b99e8f2e5cd290194fbaade2
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-04-16 15:40:14 +0200

    Adding assets and base for RecipeCard

---
+++ components/RecipeCard.js
@@ -0,0 +1,14 @@
+import { ref } from 'vue'
+import PreferenceButton from './PreferenceButton'
+
+const recipeCard = {
+    props: {
+        recipe: Object
+    },
+    setup() {
+
+    },
+    template:  `<div class="recipeCard">
+
+                </div>`
+}
