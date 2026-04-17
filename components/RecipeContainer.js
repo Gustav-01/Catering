6237dd0fe47c8b148846a40dec8dac16cea33fe8
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-04-17 13:20:29 +0200

    Changing class names to camelcase

--- components/RecipeContainer.js
+++ components/RecipeContainer.js
@@ -24,23 +24,23 @@ export default {
     template: `
         <div class="recipeContainer">
 
-            <img :src="recipe.imageUrl" :alt="recipe.imageAltText" class="recipe-image">
-                <div class="recipe-content">
+            <img :src="recipe.imageUrl" :alt="recipe.imageAltText" class="recipeImage">
+                <div class="recipeContent">
                     <h2>{{ recipe.title }} - {{ recipe.price }} SEK pp</h2>
 
-                    <div class="recipe-tags">
-                        <div class="upper-tags">
+                    <div class="recipeTags">
+                        <div class="upperTags">
                             <preference-button :title="recipe.course" background="#fff"
                                 id="typeOfCourse"></preference-button>
-                            <img :src="recipe.proteinImageUrl" class="protein-icon">
+                            <img :src="recipe.proteinImageUrl" class="proteinIcon">
                         </div>
-                        <div class="lower-tags">
+                        <div class="lowerTags">
                             <preference-button :title="recipe.theme" background="#651E81"></preference-button>
                             <preference-button v-if="recipe.modifiable" title="Anpassningsbar" background="#BC0485"></preference-button>
                         </div>
                     </div>
 
-                    <div class="recipe-actions">
+                    <div class="recipeActions">
                         <preference-button v-if="recipe.modifiable" title="Anpassa" background="#fff" id="adjust"></preference-button>
                         <preference-button title="Lägg till" background="#35BC04"></preference-button>
                     </div>
