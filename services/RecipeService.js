8b6509a2ac9cd9c7ef8eff4ee22f03d14af81ab3
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-04-17 14:28:24 +0200

    Adding more mock data and javadoc

--- services/RecipeService.js
+++ services/RecipeService.js
@@ -1,19 +1,37 @@
 import Recipe from "./Recipe.js";
 
+/**
+ * Represents a service class which would have fetched data from an API in a real scenario. This class only returns hard-coded mock data.
+ */
 class RecipeService {
-    
+
+    /**
+     * Call to generates a number of random Recipe objects of a generic nature, to use to fill out the page with mock data.
+     * 
+     * @returns an Array of random Recipe objects. 
+     */
     async getRecipes() {
+        const randomRecipes = this.generateRecipes(15);
+        return randomRecipes;
+    }
+
+    /**
+     * Call when searching for recipes. Will return an Array of two pre-filled Recipe objects.
+     * 
+     * @returns an Array of two Recipe objects.
+     */
+    async getSearchResult() {
         return [
-            new Recipe( 
-                    1,
-                    'Kyckling teriyaki', 
-                    80, 
-                    'assets/teriyaki.png',
-                    'Teriyakikyckling', 
-                    'Huvudrätt',
-                     'assets/meat.svg',
-                    'Asiatiskt',
-                    true
+            new Recipe(
+                1,
+                'Kyckling teriyaki',
+                80,
+                'assets/teriyaki.png',
+                'Teriyakikyckling',
+                'Huvudrätt',
+                'assets/meat.svg',
+                'Asiatiskt',
+                true
             ),
             new Recipe(
                 2,
@@ -27,6 +45,52 @@ class RecipeService {
                 false
             )
         ]
+    }
+
+    /**
+     * Helper method, generating generic mock Recipe objects to use as placeholders for where real data would go.
+     * 
+     * @param {Number} amountToGenerate - the number of Recipe objects to generate
+     * @returns a list of generic Recipe objects randomly generated
+     */
+    generateRecipes(amountToGenerate) {
+        const proteinsImagePaths = ['assets/meat.svg', 'assets/fish.svg', 'assets/veg.svg'];
+
+
+        const recipes = [];
+        for (let i = 1; i <= amountToGenerate; i++) {
+            const id = i;
+            const imageNumber = this.getRandomInt(1, 4);
+            const proteinIconUrl = proteinsImagePaths[this.getRandomInt(1, 4) - 1];
+            const modifiable = Math.random() < 0.5;
+
+            recipes.push(new Recipe(
+                id,
+                'Receptnamn',
+                80,
+                `assets/general-recipe-images/food${imageNumber}.png`,
+                'Maträtt',
+                'Typ av måltid',
+                proteinIconUrl,
+                'Tema',
+                modifiable
+            ));
+        }
+
+        return recipes;
+    }
+
+    /**
+     * For generating random integer. Copied from <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random">MDN</a>
+     * 
+     * @param {Number} min lowest possible (inclusive)
+     * @param {Number} max as high as, but not equal to.
+     * @returns random integer between min and max - 1
+     */
+    getRandomInt(min, max) {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
     }
 }
 
